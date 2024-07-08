7d9c64a9b77c29e3ad3bb0fe441d019ebcc772bf
wj-jason <jason.wang1709@gmail.com>
2024-07-08 19:06:03 -0400

    package

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fog',
-    version='0.1',
+    name='utmist-gpu',
+    version='0.1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'fog = fog.__main__:main',
+            'fog=utils.helper:main', 
         ],
     },
     install_requires=[
-		'python-dotenv',
 		'requests',
-		'colorama',
+        'colorama',
+        'python-dotenv'
     ],
+    description='Description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jason Wang',
+    author_email='jjh.wang@mail.utoronto.ca',
+    url='https://github.com/wj-jason/utmist-gpu',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
 )
+
